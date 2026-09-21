b3d0e56fd6c6e5a07c5f02c77d39508605d7ee12
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-21 13:10:28 -0500

    Update camera_client.h

--- client/camera_client.h
+++ client/camera_client.h
@@ -18,6 +18,6 @@ struct CAMERA_CLIENT_WIN_TAG* Client_Alloc_Camera();
 struct CAMERA_CLIENT_WIN_TAG* Create_Camera_Client( int x, int y, int w, int h, void* client );
 
 #define CLIENT_ALLOC_CAMERA() Client_Alloc_Camera()
-#define CREATE_CAMERA_CLIENT() Create_Camera_Client( 450, 450, 450, 450, &client() )
+#define CREATE_CAMERA_CLIENT() Create_Camera_Client( 450, 450, 450, 450, &CLIENT_ALLOC_CAMERA() )
 
 #endif
