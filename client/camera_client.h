677198fb165775e55107be78cfa88eb83c0c503c
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-21 13:02:15 -0500

    Update camera_client.h

--- client/camera_client.h
+++ client/camera_client.h
@@ -3,4 +3,14 @@
 #ifndef CAMERA_CLIENT_
 #define CAMERA_CLIENT_
 
+typedef struct camera_client_win_s{
+    int x;
+    int y;
+    int w;
+    int h;
+    void* client;
+}CAMERA_CLIENT_WIN_TAG;
+
+
+
 #endif
